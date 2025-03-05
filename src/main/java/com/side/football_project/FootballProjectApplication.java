785f0556fe412b9157aad1340af2b3eec6a6926e
Park JaeHong <blogerhong@naver.com>
2025-03-05 17:07:59 +0900

    add : Init Commit

---
+++ src/main/java/com/side/football_project/FootballProjectApplication.java
@@ -0,0 +1,13 @@
+package com.side.football_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/side/football_project/FootballProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.football_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
