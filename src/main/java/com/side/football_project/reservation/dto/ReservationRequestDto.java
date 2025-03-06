e7083f5da189fffe642fcd7b901ec1fee51eb4c6
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-03-06 14:36:01 +0900

    feat : "dto 생성"

---
+++ src/main/java/com/side/football_project/reservation/dto/ReservationRequestDto.java
@@ -0,0 +1,24 @@
+package com.side.football_project.reservation.dto;
+
+import com.side.football_project.domain.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@NoArgsConstructor
+public class ReservationRequestDto {
+
+    private String name;
+    private BigDecimal fee;
+    private User user;
+    private Stadium stadium;
+
+    public ReservationRequestDto(String name, BigDecimal fee, User user, Stadium stadium) {
+        this.name = name;
+        this.fee = fee;
+        this.user = user;
+        this.stadium = stadium;
+    }
+}
---
+++ src/main/java/com/side/football_project/reservation/dto/ReservationResponseDto.java
@@ -0,0 +1,39 @@
+package com.side.football_project.reservation.dto;
+
+import com.side.football_project.domain.user.entity.User;
+import com.side.football_project.reservation.domain.Reservation;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@NoArgsConstructor
+public class ReservationResponseDto {
+
+    private Long id;
+    private String name;
+    private BigDecimal fee;
+    private User user;
+    private Stadium stadium;
+
+    @Builder
+    public ReservationResponseDto(Long id, String name, BigDecimal fee, User user, Stadium stadium) {
+        this.id = id;
+        this.name = name;
+        this.fee = fee;
+        this.user = user;
+        this.stadium = stadium;
+    }
+
+    public static ReservationResponseDto toEntity(Reservation reservation) {
+        return ReservationResponseDto.builder()
+                .id(reservation.getId())
+                .name(reservation.getName())
+                .fee(reservation.getFee())
+                .user(reservation.getUser())
+                .stadium(reservation.getStadium())
+                .build();
+    }
+}
