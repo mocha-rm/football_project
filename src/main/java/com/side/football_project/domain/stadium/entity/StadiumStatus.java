1c47b9b4438ac4a3145073ed04a462be86c1c2f9
Hyojun Pyeon <vusgywns5946@gmail.com>
2025-03-10 12:22:24 +0900

    feat : 기본 CRUD 구현

---
+++ src/main/java/com/side/football_project/domain/stadium/entity/StadiumStatus.java
@@ -0,0 +1,9 @@
+package com.side.football_project.domain.stadium.entity;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum StadiumStatus {
+    AVAILABLE,
+    NOT_POSSIBLE;
+}
