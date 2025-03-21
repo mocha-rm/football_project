78c90ff303a1acd23e19de7bb224a257ee8d304d
Park JaeHong <blogerhong@naver.com>
2025-03-21 17:21:42 +0900

    update : 유저와 숏츠 연관관계 설정 및 권한 확인
    - 숏츠, 수정 및 삭제 시 권한 체크 기능 업데이트

--- src/main/java/com/side/football_project/domain/shorts/controller/ShortsController.java
+++ src/main/java/com/side/football_project/domain/shorts/controller/ShortsController.java
@@ -16,8 +16,15 @@ public class ShortsController {
     private final ShortsService shortsService;
     private final S3Service s3Service;
 
+    /**
+     * 숏츠 생성
+     * @param file 숏츠 파일
+     * @param title 숏츠 제목
+     * @param description 숏츠 설명
+     * @return 생성된 숏츠의 정보 {@link ShortsResponseDto}
+     */
     @PostMapping
-    public ResponseEntity<ShortsResponseDto> createShorts(@RequestParam("file")MultipartFile file,
+    public ResponseEntity<ShortsResponseDto> createShorts(@RequestParam("file") MultipartFile file,
                                                           @RequestParam("title") String title,
                                                           @RequestParam("description") String description) {
         String url = s3Service.uploadFile(file);
@@ -25,17 +32,32 @@ public class ShortsController {
         return ResponseEntity.ok(shortsService.createShorts(requestDto));
     }
 
+    /**
+     * 숏츠 조회
+     * @param shortsId 숏츠 ID
+     * @return 숏츠 정보
+     */
     @GetMapping("/{shortsId}")
     public ResponseEntity<ShortsResponseDto> findShorts(@PathVariable Long shortsId) {
         return ResponseEntity.ok(shortsService.findShorts(shortsId));
     }
 
+    /**
+     * 숏츠 수정
+     * @param shortsId 숏츠 ID
+     * @param requestDto 숏츠 수정에 필요한 정보 {@link ShortsRequestDto}
+     * @return 수정된 숏츠의 정보
+     */
     @PatchMapping("/{shortsId}")
     public ResponseEntity<ShortsResponseDto> updateShorts(@PathVariable Long shortsId,
                                                           @RequestBody ShortsRequestDto requestDto) {
         return ResponseEntity.ok(shortsService.updateShorts(shortsId, requestDto));
     }
 
+    /**
+     * 숏츠 삭제
+     * @param shortsId 숏츠 ID
+     */
     @DeleteMapping("/{shortsId}")
     public ResponseEntity<Void> deleteShorts(@PathVariable Long shortsId) {
         shortsService.deleteShorts(shortsId);
